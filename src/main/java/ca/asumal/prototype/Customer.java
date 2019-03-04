0c2308b5f51e52f3a97c861abf07ddaf412ad594
Arjun Sumal <arjun.sumal@gmail.com>
2019-03-03 19:03:07 -0800

    basic Java objects

---
+++ src/main/java/ca/asumal/prototype/Customer.java
@@ -0,0 +1,26 @@
+package ca.asumal.prototype;
+
+public class Customer {
+    private final String name;
+    private Integer refund;
+
+    public Customer(String name) {
+        this.name = name;
+        refund = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void refund(Integer price) {
+        refund += price;
+    }
+
+    public Integer getRefunded() {
+        Integer result = refund;
+        refund = 0;
+
+        return result;
+    }
+}
---
+++ src/main/java/ca/asumal/prototype/Item.java
@@ -0,0 +1,21 @@
+package ca.asumal.prototype;
+
+public class Item {
+    private final String itemName;
+
+    private final Integer price;
+
+
+    public Item(String itemName, Integer price) {
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+}
